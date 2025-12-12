82f57213e97b01a5d2d27be9cda8f4e59f0b7b8a
snowman6-git <chldlfgks55@gmail.com>
2025-12-12 22:45:07 +0900

    인커밍 아이피 예외처리 & 핸드쉐이크 간단구현

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::{net::{TcpListener, TcpStream}, thread};
+use std::{io::Write, net::{TcpListener, TcpStream}, thread};
 
-fn handle_connection(stream: TcpStream){
+fn handle_connection(mut stream: TcpStream){
+    let ip = stream.peer_addr();
+    //소켓에 보내는거 만으로 연결이 됐음을 확정지을수 있는지 확인 ex. 연결이 끊어져도 보내지는가 (보내는거만으로 연결이 됌을 확정지을 수 있는가)
+    let handshake_msg = stream.write("aa2iswork".as_bytes());
+    match handshake_msg {
+        Ok(_) => {
+            println!("{}: HandShake Complete!", ip.unwrap())
+        },
+        Err(er) => {println!("{}: {}", ip.unwrap(), er)}  
+    }
+    
+    loop {
+        let mut msg = String::from("");
+        std::io::stdin().read_line(&mut msg).unwrap();
+        stream.write(msg.as_bytes()).unwrap();
+    }
 }
 
 fn listen_up(master_addr: &str) -> TcpListener{
@@ -36,7 +51,15 @@ fn main() {
         // 스레드 생성
         thread::spawn(|| {
             // 들어온 애 주소
-            println!("{} is connected!", stream.peer_addr().unwrap());
+            let incoming_ip_read = stream.peer_addr();
+            match incoming_ip_read {
+                Ok(ip) => {
+                    println!("{} is incoming...", ip);
+                },
+                Err(error) => {
+                    println!("{}", error)
+                }
+            };
             // 절로 던져
             handle_connection(stream);
         });
