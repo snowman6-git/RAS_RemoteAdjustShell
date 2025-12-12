a43b5d7b1c50c00bc1b0da9212229ddbfc76a73b
snowman6-git <chldlfgks55@gmail.com>
2025-12-13 02:09:56 +0900

    테스트 환경에서 몰랐던 OS간 타임아웃 시간에 따른 교정 리커넥트 함수화 및 connect -> connect_timeout

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,28 @@ use std::{net::TcpStream, io::ErrorKind};
 use std::{str::from_utf8};
 use std::thread::{sleep};
 
+fn reconnect(){
+    let mut reconnect_ment = "try to connect.".to_string();
+    for i in 1..=3 { //3초 다 채우고 연결하게 할건지 1초에 한번씩 연결시도 할건지 고민하기 
+        print!("{}", reconnect_ment);
+        reconnect_ment.push('.');
+        io::stdout().flush().unwrap();
+        print!("\r"); 
+        sleep(Duration::from_secs(1));
+    }
+    print!("\r\x1b[K");
+}
+
 fn main() {
-    let address: &str = "localhost:9999";
+    // let address: &str = "localhost:9999";
+    let address = "175.206.180.193:9999".parse().unwrap(); //차후 오류/예외처리하기
     println!("{}", address);
     'master_connect: loop {
-        match TcpStream::connect(&address) {
+        match TcpStream::connect_timeout(&address, Duration::from_secs(1)) {
             Ok(mut stream) => {
-                
                 println!("Connected!");
                 let mut data = [0 as u8; 1000]; 
                 'streaming: loop {
-
                     match stream.read(&mut data){
                         Ok(bsize) => {
                             if bsize == 0 {
@@ -26,12 +37,15 @@ fn main() {
                             println!("{}", text); // 디버깅을 위해 text가 무엇인지 확인해볼 수 있음
                         },
                         Err(error) => match error.kind(){
-                            ErrorKind::ConnectionRefused => {
+                            ErrorKind::ConnectionRefused | ErrorKind::ConnectionReset => {
+                                println!("{}", error);
                                 println!("signal lost from: {}", address);
                                 break 'streaming;
                             },
-                            _ => {}
-                            // println!("{}", error);
+                            _ => {
+                                println!("{}", error);
+                            }
+                            
                             // break 'streaming;
 
                         }
@@ -39,23 +53,12 @@ fn main() {
                 }
             },
             Err(error) => match error.kind(){
-                ErrorKind::ConnectionRefused => { 
-                    let mut reconnect_ment = "try to connect.".to_string();
-                    for i in 1..=3 { //3초 다 채우고 연결하게 할건지 1초에 한번씩 연결시도 할건지 고민하기 
-                        print!("{}", reconnect_ment);
-                        reconnect_ment.push('.');
-                        io::stdout().flush().unwrap();
-                        print!("\r"); 
-                        sleep(Duration::from_secs(1));
-                    }
-
-                    // 루프 종료 후, 싹 비워줌
-                    print!("\r\x1b[K");
-
-                    // 루프 종료 후, 다음 출력이 덮어쓰지 않도록 줄바꿈을 한번 해줌
-                    // println!();
+                ErrorKind::TimedOut | ErrorKind::ConnectionRefused | ErrorKind::ConnectionReset => { 
+                    reconnect();
                 },
-                _ => {}
+                _ => {
+                    println!("{}", error);
+                }
             }
         }
     }
